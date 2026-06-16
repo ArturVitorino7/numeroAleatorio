921187ab22d06ac3ded7aa8cd473a2c7e4c842a1
Artur Vitorino <arturvitorinodasilva2@gmail.com>
2026-06-16 11:24:45 -0300

    deixando o codigo mais facil

--- src/JogoAdvinha.java
+++ src/JogoAdvinha.java
@@ -5,12 +5,12 @@ public class JogoAdvinha {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         Random random = new Random();
-        int numeroSecreto = random.nextInt(100);
+        int numeroSecreto = random.nextInt(50);
         int palpite = 0;
 
         int tentativas = 0;
         for (tentativas = 1; tentativas <= 5; tentativas++){
-            System.out.println("Digite um numero de 0 a 100");
+            System.out.println("Digite um numero de 0 a 50");
             palpite = entrada.nextInt();
             if (numeroSecreto == palpite){
                 System.out.println("Parabens você acertou");
